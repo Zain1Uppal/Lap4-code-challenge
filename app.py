39f2e15249c862ad6b7ee822ecd301315434a6c9
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-12-01 14:47:25 +0000

    refactor empty input url handling to the form itself

--- app.py
+++ app.py
@@ -31,9 +31,6 @@ def home():
 def generate():
     if request.method == 'POST':
         received_url = request.form['input_url']
-        if not received_url:
-            # Don't continue if an empty string is inputted
-            return render_template('form.html', shortUrl= '', title='Generator')
         # Generate short url identifier and add data to database
         short_url = ''.join(random.choices(string.ascii_letters + string.digits, k = 5))
         db.session.add(Shorturl(url=received_url, shortenUrl=short_url))
